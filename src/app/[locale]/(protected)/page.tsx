d166d3d0138065273a64562459c185eded2e9334
DinoDev <honghai251.info@gmail.com>
2025-11-21 15:31:51 +0700

    Fix build error: Separate client component in (protected)/page.tsx

--- src/app/[locale]/(protected)/page.tsx
+++ src/app/[locale]/(protected)/page.tsx
@@ -1,11 +1,5 @@
-"use client";
-import ExampleComponents from "@/components/ExampleComponents";
+import Homepage from "./page-client";
 
-export default function Homepage() {
-  return (
-    <div>
-      <h2 className="mb-3">Example components</h2>
-      <ExampleComponents />
-    </div>
-  );
+export default function HomepagePage() {
+  return <Homepage />;
 }
